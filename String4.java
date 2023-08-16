b2c5aa744543939c366b6d3072a00b07c0715d15
Prashant Singh <134587691+prashantsingh05@users.noreply.github.com>
2023-08-16 21:01:24 +0530

    Add files via upload

---
+++ String4.java
@@ -0,0 +1,9 @@
+public class String4 {
+    public static void main(String[] args) {
+        // String
+        // replacement 
+        String name = "Prashant";
+        String name2 = name.replace('a' , 'p');
+        System.out.println(name2);
+    }
+}
